e3f1af0c364e591cc2d539d6958708f0d9131999
codeiffor <siddhartha.trivedi@smallcase.com>
2022-12-18 17:11:01 +0530

    day 15

--- 15_java/1.java
+++ 15_java/1.java
@@ -5,6 +5,16 @@ public class Main {
 		int part1Y = 2000000;
 		HashSet<Integer> noBeacon = new HashSet();
 		HashSet<Integer> beacon = new HashSet();
+
+		int part2Size = 4000001;
+		ArrayList<Integer>[] xRangeS = new ArrayList[part2Size];
+		ArrayList<Integer>[] xRangeE = new ArrayList[part2Size];
+		for (int i = 0; i < part2Size; i ++) {
+			xRangeS[i] = new ArrayList<Integer>();
+			xRangeE[i] = new ArrayList<Integer>();
+		}
+
+
 		Scanner in = new Scanner(System.in);
 		while (in.hasNext()) {
 			String line = in.nextLine();
@@ -21,11 +31,11 @@ public class Main {
 			int bx = l.nextInt();
 			l.next();
 			int by = l.nextInt();
-			
+
+			// part 1
 			if (by == part1Y) {
 				beacon.add(bx);
 			}
-
 			int distance = calcDistance(sx, sy, bx, by);
 			int yDiff = Math.abs(part1Y - sy);
 			int xOffset = distance - yDiff;
@@ -35,9 +45,43 @@ public class Main {
 				}
 			}
 
+			// part 2
+			for (int i = sx - distance; i <= sx + distance; i ++) {
+				int yOffset = distance - Math.abs(sx - i);
+				if (i >= 0 && i < part2Size && sy + yOffset >= 0 && sy - yOffset < part2Size) {
+					xRangeS[i].add(Math.max(sy - yOffset, 0));
+					xRangeE[i].add(Math.min(sy + yOffset, part2Size - 1));
+				}
+			}
 		}
 		System.out.print("Part 1: ");
 		System.out.println(noBeacon.size() - beacon.size());
+
+		int x = -1;
+		int y = -1;
+		outerloop:
+		for (int i = 0; i < part2Size; i ++) {
+			for (int j = 0; j < part2Size; ) {
+				int maxJ = j;
+				boolean found = false;
+				for (int k = 0; k < xRangeS[i].size(); k ++) {
+					if (xRangeS[i].get(k) <= j && xRangeE[i].get(k) >= j) {
+						found = true;
+						if (maxJ < xRangeE[i].get(k)) {
+							maxJ = xRangeE[i].get(k);
+						}
+					}
+				}
+				if (!found) {
+					x = i;
+					y = j;
+					break outerloop;
+				}
+				j = maxJ + 1;
+			}
+		}
+		System.out.print("Part 2: ");
+		System.out.println(4000000 * Long.valueOf(x) + Long.valueOf(y));
 	}
 	public static int calcDistance(int sx, int sy, int bx, int by) {
 		return Math.abs(sx - bx) + Math.abs(sy - by);
